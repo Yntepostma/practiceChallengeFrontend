5273743809a1ae21bda567f7ed45e430bc969b7b
YntepostmaE <ynte.postma@gmail.com>
2022-11-08 14:59:52 +0100

    final commit

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addNewPost } from "../store/user/thunks";
 import { selectPostMessage } from "../store/user/selectors";
+import "./style.css";
 
 export const PostPage = () => {
   const dispatch = useDispatch();
@@ -27,48 +28,55 @@ export const PostPage = () => {
 
   return (
     <div>
-      <h2>Add a new Post</h2>
-      <h4>{!message ? "" : message}</h4>
+      <div>
+        <div className="container">
+          <h2>Add a new Post</h2>
+          <h4>{!message ? "" : message}</h4>
 
-      <form onSubmit={handleSubmit}>
-        <label>
-          <h4>Name of Post:</h4>
-          <input
-            type="text"
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label>
-          <h4>Content:</h4>
-          <input
-            type="text"
-            placeholder="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <label id="image-preview">
-          <h4>Image URL:</h4>
-          <input
-            // type="file"
-            placeholder="imageUrl"
-            value={imageUrl}
-            accept="image/*"
-            id="choose-file"
-            name="choose-file"
-            onChange={(e) => setImageUrl(e.target.value)}
-          />
-        </label>
-        <br></br>
-        <button type="Submit">POST</button>
-      </form>
-      {imageUrl && (
-        <img style={{ width: 200, height: "auto" }} alt={name} src={imageUrl} />
-      )}
+          <form className="form" onSubmit={handleSubmit}>
+            <label>
+              <h4>Name of Post:</h4>
+              <input
+                type="text"
+                placeholder="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <br></br>
+            <label>
+              <h4>Content:</h4>
+              <input
+                type="text"
+                placeholder="content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            </label>
+            <br></br>
+            <label id="image-preview">
+              <h4>Image URL:</h4>
+              <input
+                placeholder="imageUrl"
+                value={imageUrl}
+                accept="image/*"
+                id="choose-file"
+                name="choose-file"
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </label>
+            <br></br>
+            <button type="Submit">POST</button>
+          </form>
+          {imageUrl && (
+            <img
+              style={{ width: 200, height: "auto" }}
+              alt={name}
+              src={imageUrl}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
